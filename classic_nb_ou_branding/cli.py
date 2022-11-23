0b8472356dbc9b0e4f66bdd5997df581f6a4ffb9
Tony Hirst <tony.hirst@gmail.com>
2022-11-23 15:38:03 +0000

    Use click

--- classic_nb_ou_branding/cli.py
+++ classic_nb_ou_branding/cli.py
@@ -1,18 +1,44 @@
+import click
+
 import shutil
 import sys
 import subprocess
 from pathlib import Path
+import jupyter_core
 
-def install():
+@click.group()
+def cli():
+	pass
+
+@cli.command()
+@click.option('--conda', '-c',  help='Name of conda environment')
+def install(conda):
     """Install OU branding."""
     #print("Installing OU branding to Jupyter environment.")
     pkgdir = sys.modules['classic_nb_ou_branding'].__path__[0]
     fullpath = Path(pkgdir) / "resources"
-    dest = subprocess.run(['jupyter', '--config-dir'], stdout=subprocess.PIPE)
-    outpath = Path(dest.stdout.decode('utf-8').strip()) / "custom"
+    
+    # TO DO - should we get the config via:
+    # import jupyter_core
+    # jupyter_core.paths.jupyter_config_dir()
+    # Or maybe the following, which gives env paths:
+    # jupyter_core.paths.jupyter_config_path()
+    # Then have a cli arg that lets you install to an env?
+    # eg list includes:
+    # '/Users/tonyhirst/opt/miniconda3/envs/r_env/etc/jupyter',
+    
+    if conda:
+        paths = jupyter_core.paths.jupyter_config_path()
+        for path in paths:
+            if f"envs/{conda}" in path:
+                dest = path
+                break
+    else:
+        dest = subprocess.run(['jupyter', '--config-dir'], stdout=subprocess.PIPE)
+        dest = Path(dest.stdout.decode('utf-8').strip())
+    outpath = dest / "custom"
     outpath.mkdir(parents=True, exist_ok=True)
     for f in fullpath.rglob("*"):
         #print(f"copy {f} to {outpath}")
         rf = shutil.copy(f, outpath)
         #print(f"returned {rf}")
-    
--- setup.py
+++ setup.py
@@ -11,12 +11,12 @@ setup(
     A simple "dummy" package that installs custom branding elements for classic Jupyter notebook.
     ''',
     long_description_content_type="text/markdown",
-    install_requires=[],
+    install_requires=["click"],
     include_package_data=True,
     package_data={'': ['resources/*']},
     entry_points='''
         [console_scripts]
-        ou_install_nb_branding=classic_nb_ou_branding.cli:install
+        ou_nb_branding=classic_nb_ou_branding.cli:cli
     '''
 )
 
